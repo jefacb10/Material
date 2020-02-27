af59450b397efc07e34da9c13a6cc4ef2da84d85
jefacb10 <32496187+jefacb10@users.noreply.github.com>
2020-02-26 23:14:57 -0300

    commit

---
+++ materialapp/src/app/material/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule
+} from '@angular/material';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
